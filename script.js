c7fc6a136859e535b865785f4a87629d9597eee3
samuel <samueldacosta479@gmail.com>
2025-03-05 15:49:46 -0300

    avaliacao2.1

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const ctx = canvas.getContext('2d')
 class Entidade {
     #gravidade
     constructor(propriedades){
-        this.x = x;
-        this.y = y;
-        this.largura = largura;
-        this.altura = altura
+        this.x = propriedades.x;
+        this.y = propriedades.y;
+        this.largura = propriedades.largura;
+        this.altura = propriedades.altura
         this.#gravidade=0.5
     }
     desenhar = function (ctx, cor) {
@@ -18,3 +18,59 @@ class Entidade {
         return this.#gravidade;
     }
 }
+
+class Raquete extends Entidade {
+    constructor(propriedades) {
+        super(propriedades)
+        this.velocidade = 7
+        this.direcao = 0
+    }
+
+
+    moverRaquete() {
+        if (this.direcao == 1 && this.x > 0) {
+            this.x -= this.velocidade
+        } else if (this.direcao == -1 && this.x + this.largura < canvas.width) {
+            this.x += this.velocidade
+        }
+    }
+    
+    setDirecao(direcao) {
+        this.direcao = direcao;
+    }
+}
+const raquete = new Raquete({
+        x: 100,
+        y: canvas.height - 20,
+        largura: 80,
+        altura: 10
+    })
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'KeyA') {
+        raquete.setDirecao(1)
+    } else if (e.key == 'keyD') {
+        raquete.setDirecao(-1)
+    }
+});
+
+document.addEventListener('keyup', () => {
+    raquete.setDirecao(0)
+})
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    raquete.mover()
+    raquete.desenhar(ctx, 'blue')
+
+    requestAnimationFrame(atualizar)
+
+
+
+function loop(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    raquete.mover()
+    raquete.desenhar(ctx, 'white')
+    requestAnimationFrame(loop())
+    
+}
+loop()
