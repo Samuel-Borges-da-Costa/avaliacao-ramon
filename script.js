158de95e2d3bdca7cca83a9b1751ef9b89b5f80d
samuel <samueldacosta479@gmail.com>
2025-03-05 16:10:15 -0300

    avaliacao2.3

--- script.js
+++ script.js
@@ -39,6 +39,42 @@ class Raquete extends Entidade {
         this.direcao = direcao;
     }
 }
+
+
+class Bola extends Entidade {
+    #velocidadeX
+    #velocidadeY
+    constructor(propriedades) {
+        super(propriedades)
+        this.velocidadeX = 4
+        this.velocidadeY = -4
+    }
+
+    moverBola() {
+        this.x += this.velocidadeX
+        this.y += this.velocidadeY
+    }
+
+    verificarColisaoRaquete(raquete) {
+        if (this.x > raquete.x && this.x < raquete.x + raquete.largura &&
+            this.y + this.altura > raquete.y && this.y < raquete.y + raquete.altura) {
+            this.velocidadeY = -this.velocidadeY
+        }
+    }
+
+    verificarColisaoParedes() {
+        if (this.x < 0) {
+            this.velocidadeX = -this.velocidadeX
+        }
+        if (this.x + this.largura > canvas.width) {
+            this.velocidadeX = -this.velocidadeX
+        }
+        if (this.y < 0) {
+            this.velocidadeY = -this.velocidadeY
+        }
+    }
+}
+
 const raquete = new Raquete({
         x: 100,
         y: canvas.height - 20,
@@ -47,9 +83,9 @@ const raquete = new Raquete({
     })
 
 document.addEventListener('keydown', (e) => {
-    if (e.key == 'KeyA') {
+    if (e.key == 'a') {
         raquete.setDirecao(1)
-    } else if (e.key == 'keyD') {
+    } else if (e.key == 'd') {
         raquete.setDirecao(-1)
     }
 });
@@ -57,20 +93,23 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('keyup', () => {
     raquete.setDirecao(0)
 })
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    raquete.mover()
-    raquete.desenhar(ctx, 'blue')
-
-    requestAnimationFrame(atualizar)
-
-
+const bola = new Bola({
+    x: canvas.width / 2 - 10,
+    y: canvas.height - 40,
+    largura: 20,
+    altura: 20
+});
 
 function loop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    raquete.mover()
+    raquete.moverRaquete()
     raquete.desenhar(ctx, 'white')
-    requestAnimationFrame(loop())
+    bola.moverBola()
+    bola.verificarColisaoRaquete(raquete)
+    bola.verificarColisaoParedes()
+    bola.desenhar(ctx, 'red')
+    requestAnimationFrame(loop)
     
 }
 loop()
